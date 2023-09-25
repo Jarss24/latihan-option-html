59da5976fe79c43ed6389a3e12949beb07e1d6e6
Jarss24 <141621402+Jarss24@users.noreply.github.com>
2023-09-25 16:35:59 +0700

    html
    P

---
+++ js.js
@@ -0,0 +1,11 @@
+name1 = 'Fadhlan'
+name2 = 'Anggara'
+name3 = 'Dika'
+name4 = 'Adip'
+
+const ueue = function(){
+    console.log('Hiya ' + name1 + '!!', '\nHi ' + name2 + '!!',
+    '\nHeyo ' + name3 + '!!', '\nHello ' + name4 + '!!')
+}();
+
+//we did it, we did it, hooray hooray!! *dora theme plays*
